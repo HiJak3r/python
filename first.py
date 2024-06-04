de6c003346b732aa97c87c113ef31710d0694e5c
HiJak3r <83827273+HiJak3r@users.noreply.github.com>
2024-06-04 11:38:47 -0400

    Create my first .py script

---
+++ first.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+# You can print a string
+print("Hello world")
+
+# You can also use single quotes for a string
+print('Hello world')
+
+# You can use triple quotes to print over multiple lines
+print("""This string runs
+multiple lines!""")
+
+# You can also concatenate strings together
+print("This string is "+"awesome!")
+
+# This is how you'd make a new line
+print('\n')
+print('Test that new line out.')
