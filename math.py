31356af3303cd438b1b6722a3068e2176cdf0578
HiJak3r <83827273+HiJak3r@users.noreply.github.com>
2024-06-04 15:39:08 -0400

    Create py script to demo math

---
+++ math.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+# You can add integers
+print(50 + 50)
+
+# You can subtract integers
+print(50 - 50)
+
+# You can multiply integers
+print(50 * 50)
+
+# You can divide intigers - This will also create what's called a float which is simply a decimal even if it's only 1.0
+print(50 / 50)
+
+# Python will automagically do PEMDAS for you
+print(50 + 50 - 50 * 50 / 50)
+
+# If you want an exponent you can use this
+print(50 ** 2)
+
+# The % sign is called a modulo - this simply gives you what is left over aka the remainder
+print(50 % 6)
+
+# This will give allow you to divide with the remainder
+print(50 / 6)
+
+# If you want no remainder, you can do this
+print(50 // 6)
