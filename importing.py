9b5e8bacb5352993b0bb445a3652f34377c4b3b7
HiJak3r <83827273+HiJak3r@users.noreply.github.com>
2024-06-04 18:29:44 -0400

    Create demo to show importing modules

---
+++ importing.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+
+# IMPORTING - Importing is important
+import sys # for system functions and parameters
+from datetime import datetime as dt # You can import specific things from modules and not the whole module then even add an alias
+
+print(sys.version)
+
+print(dt.now())
